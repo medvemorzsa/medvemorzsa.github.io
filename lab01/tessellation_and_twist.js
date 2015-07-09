473447ca28f33d93418856ee19567ebef2d002a2
delta\krichard <richard.kovacs@delta.hu>
2015-07-09 12:40:16 +0200

    Add getter and setter to subdivision parameter

--- lab01/tessellation_and_twist.js
+++ lab01/tessellation_and_twist.js
@@ -1,4 +1,5 @@
 function App(maxSubdivision) {
+    // Constants
     this.types = {
         WIREFRAMED: 0,
         PARTIALLY_FILLED: 1,
@@ -10,11 +11,29 @@ function App(maxSubdivision) {
     
     var maxSubdivision = (typeof maxSubdivision !== 'undefined') ?  maxSubdivision : 10;
     
+    // User-modifiable parameters
     var subdivision = 5;
     var angle = 0;
     var tessellation = true;
     var style = this.types.WIREFRAMED;
     var color = vec4( 0.0, 0.0, 0.0, 1.0 );    
+    
+    // Getter for subdivision parameter
+    this.__defineGetter__("subdivision", function() {
+        return subdivision;
+    });
+    
+    // Setter for subdivision parameter
+    this.__defineSetter__("subdivision", function(value) {
+        if (typeof value === "undefined")
+            throw "Number of subdivision is required!";
+        if ((isNaN(value)) || (!(Number.isInteger(value))))
+            throw "Number of subdivision must be integer value!";
+        if ((value < 0) || (value > maxSubdivision))
+            throw "Number of subdivision must be between 0 and " + maxSubdivision + "!";
+        subdivision = value;
+    });
+    
 }
 
 Number.isInteger = Number.isInteger || function(value) {
