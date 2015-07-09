0963bd282bc47158ea1b6c631a0a5a48b98f70ae
delta\krichard <richard.kovacs@delta.hu>
2015-07-09 12:03:07 +0200

    Setup user-modifiable parameters of app

--- lab01/tessellation_and_twist.js
+++ lab01/tessellation_and_twist.js
@@ -1,4 +1,20 @@
-function App() {
+function App(maxSubdivision) {
+    this.types = {
+        WIREFRAMED: 0,
+        PARTIALLY_FILLED: 1,
+        FILLED: 2
+    }
+    
+    if (Object.freeze)
+        Object.freeze(this.types);
+    
+    var maxSubdivision = (typeof maxSubdivision !== 'undefined') ?  maxSubdivision : 10;
+    
+    var subdivision = 5;
+    var angle = 0;
+    var tessellation = true;
+    var style = this.types.WIREFRAMED;
+    var color = vec4( 0.0, 0.0, 0.0, 1.0 );    
 }
 
 $(document).ready(function(){
