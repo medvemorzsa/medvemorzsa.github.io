c149547ae6427c9ec4d74e3e9015005017c83349
delta\krichard <richard.kovacs@delta.hu>
2015-08-12 10:56:02 +0200

    Start Lab #03

---
+++ lab03/CAD.js
@@ -0,0 +1,61 @@
+var app = angular.module("webGlLab03App", []);
+
+app.controller("webGlLab03Ctrl", function($scope) {
+    $scope.varLoading = true;
+    
+    $scope.canvas = null;
+
+    // WebGL initialization
+    $scope.initWebGL = function() {
+        // Configure canvas and WebGL
+        $scope.canvas = document.getElementById("gl-canvas");
+        if (($scope.canvas == null) || ($scope.canvas == "undefined"))
+            return false;
+        $scope.gl = WebGLUtils.setupWebGL($scope.canvas);
+        if (!$scope.gl) {
+            alert( "WebGL isn't available" );
+            return false;
+        }
+
+        $scope.gl.viewport(0, 0, $scope.canvas.width, $scope.canvas.height);        
+        
+        $scope.gl.clearColor(1.0, 1.0, 1.0, 1.0);
+        
+        return true;
+    }
+    
+    // Render function
+    $scope.render = function() {
+        if (!($scope.gl)) return;
+        $scope.gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    };
+    
+    if ($scope.initWebGL()) {   
+        $scope.render();
+        $scope.varLoading = false;
+    }
+});
+
+// Resize canvas
+app.directive('resize', function ($window) {
+    return function (scope, element) {
+        var w = angular.element($window);
+
+        scope.getWindowDimensions = function () {
+            return {
+                'h': element.height(),
+                'w': element.width()
+            };
+        };
+        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+            scope.canvas.width = Math.min(newValue.w, newValue.h);
+            scope.canvas.height = Math.min(newValue.w, newValue.h);
+            scope.gl.viewport(0, 0, scope.canvas.width, scope.canvas.height);
+            scope.render();
+        }, true);
+
+        w.bind('resize', function () {
+            scope.$apply();
+        });
+    }
+});
